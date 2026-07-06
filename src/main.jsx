e815980fd4926fea273a541c0dfa50e514137199
Baliappp <tbibi555@gmail.com>
2026-07-06 11:28:46 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BaliApp from "./BaliApp.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BaliApp />
+  </React.StrictMode>
+);
